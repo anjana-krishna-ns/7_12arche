80195a304d606990aa8414b71c34cf92554f6ad5
anjana-krishna-ns <118959494+anjana-krishna-ns@users.noreply.github.com>
2022-12-07 16:46:07 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'demo/demo.dart';
+import 'demo/home_demo.dart';
+import 'demo/test.dart';
+import 'newarch/log_in/log_in_page.dart';
+import 'newarch/screens/home page/banner.dart';
+import 'newarch/screens/home page/grid.dart';
+import 'newarch/screens/home page/home.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Banner1(),
+    );
+  }
+}
